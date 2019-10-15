a60802184075665ddfc4d04c9b549609efce5457
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2019-10-15 17:09:02 -0300

    new release version 2.0.0

--- calc/math.go
+++ calc/math.go
@@ -1,12 +1,19 @@
 package calc
 
+import "errors"
+
 // returns sum of two integers
-func Add(numbers ...int) int {
+func Add(numbers ...int) (error, int) {
 	sum := 0
 
+	if len(numbers) < 2 {
+		return errors.New("provide more than 2 numbers"), sum
+	} else {
 	for _, num := range numbers {
 		sum = sum + num
 	}
 
-	return sum
+
+	return nil, sum
+	}
 }
